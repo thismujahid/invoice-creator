45409f44ec27935b5fe140644b7d1464647a6e9f
Muhammad Mujahid <mohamed.mojahead@gmail.com>
2025-10-20 21:53:17 +0300

    reactivate SSR Mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ import { ar } from 'vuetify/locale';
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-  ssr:false,
   modules: ["vuetify-nuxt-module", "@pinia/nuxt", "@vite-pwa/nuxt"],
   nitro: {
     prerender: {
