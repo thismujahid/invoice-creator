675932c2ea343563a319b516e69524fc9b41f35e
Mohamed Mujahid <Mohamed.mojahead@gmail.com>
2026-04-13 15:30:51 +0200

    remove logs

--- composables/firebase.js
+++ composables/firebase.js
@@ -52,7 +52,6 @@ async function readFrom(module, filters = {}) {
         q = query(q, where("created_by", "==", value));
       } else if (key === 'remaining' && value) {
         q = query(q, where(key, ">=", 0.1));
-        console.log("🚀 ~ readFrom ~ q:", q)
       }else if (value !== undefined && value !== null && value !== "") {
         q = query(q, where(key, "==", value));
       }
